efa01d54948353937e15cab1a7fa045d401b7605
ErikBoesen <me@erikboesen.com>
2019-02-08 21:23:34 -0500

    Expand token explanation

--- groupme_analytics.py
+++ groupme_analytics.py
@@ -5,11 +5,11 @@ from pprint import pprint
 
 at = ""  # this is a global variable that stores the API token
 
-
 def menu():
     global at
     print('If you have not done so already, go to the following website to receive your API token: ' +
-          'https://dev.groupme.com/. When signing up, it does not matter what you put for the callback URL')
+          'https://dev.groupme.com/. When signing up, it does not matter what you put for the callback URL.' +
+          'Alternately, click "Access Token" to use your account for authentication.')
     at = str(raw_input("Enter your developer access token:"))
     print("Here are your ten most recent groups:")
     groups_data = print_all_groups_with_number_beside_each()
